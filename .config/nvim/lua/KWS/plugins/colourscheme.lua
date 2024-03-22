676c2dda5098a5a8afb10b43a753100cea36a80d
WooSeok Kim <3suksw@gmail.com>
2024-03-22 12:49:30 +0900

    change of colourscheme: to everforest

--- .config/nvim/lua/KWS/plugins/colourscheme.lua
+++ .config/nvim/lua/KWS/plugins/colourscheme.lua
@@ -1,12 +1,15 @@
 local M = {
-	"folke/tokyonight.nvim",
+	"neanias/everforest-nvim",
 	lazy = false,
 	priority = 1000,
 }
 
 function M.config()
-	--vim.cmd([[colorscheme tokyonight-night]])
-	vim.cmd([[colorscheme tokyonight-moon]])
+	-- vim.cmd([[colorscheme tokyonight-moon]])
+	require("everforest").setup({
+		background = "hard",
+	})
+	vim.cmd([[colorscheme everforest]])
 end
 
 return M
