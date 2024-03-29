dcfe3583c12f056603b6342af26a8bf34075aa43
WooSeok Kim <3suksw@gmail.com>
2024-03-29 21:59:01 +0900

    update todo-comments.lua

--- .config/nvim/lua/KWS/plugins/todo-comments.lua
+++ .config/nvim/lua/KWS/plugins/todo-comments.lua
@@ -2,6 +2,14 @@ return {
 	"folke/todo-comments.nvim",
 	dependencies = { "nvim-lua/plenary.nvim" },
 	opts = {
+		colors = {
+			error = { "DiagnosticError", "ErrorMsg", "#E67E80" },
+			warning = { "DiagnosticWarn", "WarningMsg", "#DBBC7F" },
+			info = { "DiagnosticInfo", "#7FBBB3" },
+			hint = { "DiagnosticHint", "#63C092" },
+			default = { "Identifier", "#4C3743" },
+			test = { "Identifier", "#D699B6" },
+		},
 		-- your configuration comes here
 		-- or leave it empty to use the default settings
 		-- refer to the configuration section below
