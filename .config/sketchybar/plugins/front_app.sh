2dd07350208ac1d4ebe8b1429d8baaab9e0151e1
WooSeok Kim <3suksw@gmail.com>
2024-04-21 21:46:22 +0900

    add animation

--- .config/sketchybar/plugins/front_app.sh
+++ .config/sketchybar/plugins/front_app.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 if [ "$SENDER" = "front_app_switched" ]; then
-  sketchybar --set $NAME label="$INFO" icon="$($CONFIG_DIR/plugins/icon_map_fn.sh "$INFO")"
+  sketchybar --animate sin 10 --set $NAME label="$INFO" icon="$($CONFIG_DIR/plugins/icon_map_fn.sh "$INFO")"
 fi
--- .config/sketchybar/plugins/space.sh
+++ .config/sketchybar/plugins/space.sh
@@ -3,11 +3,11 @@
 source "$CONFIG_DIR/colours.sh"
 
 if [ $SELECTED = true ]; then
-  sketchybar --set $NAME background.drawing=on \
+  sketchybar --animate exp 10 --set $NAME background.drawing=on \
                          label.color=$TEXT_COLOUR \
                          icon.color=$TEXT_COLOUR 
 else
-  sketchybar --set $NAME background.drawing=on \
+  sketchybar --animate exp 10 --set $NAME background.drawing=on \
                          label.color=$GREY \
                          icon.color=$GREY
 fi
