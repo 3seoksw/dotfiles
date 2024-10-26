254333e71362440a4e40036a9e6da5710f914c10
WooSeok Kim <3suksw@gmail.com>
2024-10-26 21:53:48 +0900

    save multiple colourschemes into directory

---
+++ .config/nvim/lua/KWS/plugins/colourschemes/kanagawa.lua
@@ -0,0 +1,20 @@
+return {
+	"rebelot/kanagawa.nvim",
+	lazy = false,
+	config = function()
+		require("kanagawa").setup({
+			compile = true,
+			overrides = function(colors)
+				local theme = colors.theme
+				return {
+					FloatBorder = { bg = theme.ui.bg_p1 },
+					Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 }, -- add `blend = vim.o.pumblend` to enable transparency
+					PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
+					PmenuSbar = { bg = theme.ui.bg_m1 },
+					PmenuThumb = { bg = theme.ui.bg_p2 },
+				}
+			end,
+		})
+		vim.cmd("colorscheme kanagawa-wave")
+	end,
+}
